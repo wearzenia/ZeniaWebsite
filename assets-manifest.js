1a957fbf823cd987a6261189751f4010c1a199f0
wearzenia <zenianightwear@gmail.com>
2026-07-06 00:01:44 +0530

    Create assets-manifest.js

---
+++ assets-manifest.js
@@ -0,0 +1,30 @@
+window.COLLECTION_IMAGES = {
+
+    nighties: [
+        "Assets/nighties/n1.jpg",
+        "Assets/nighties/n2.jpg",
+        "Assets/nighties/n3.jpg"
+    ],
+
+    kaftans: [
+        "Assets/kaftans/k1.jpg",
+        "Assets/kaftans/k2.jpg"
+    ],
+
+    coords: [
+        "Assets/coords/c1.jpg",
+        "Assets/coords/c2.jpg",
+        "Assets/coords/c3.jpg",
+        "Assets/coords/c4.jpg"
+    ],
+
+    nightsuits: [],
+
+    midi: [],
+
+    "feeding-nighty": [],
+
+    "short-kaftan": [],
+
+    fancy: []
+};
